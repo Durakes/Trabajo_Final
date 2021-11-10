d51c88b24bb63b8abc9dd311776eb7a609b02402
Arnold Pajuelo <arnold.pajuelo@gmail.com>
2021-11-10 00:26:20 -0500

    Improves
    + Cleaning code
    + Improve import name
    + Added month in the register

--- main/windows_app/register_window.py
+++ main/windows_app/register_window.py
@@ -1,22 +1,12 @@
 from tkinter import *
 from tkinter import ttk
 from tkcalendar import *
-import windows_app.dashboard_window as dw
+import windows_app.dashboard_window as dashboard_w
 import os
-
 #! Variables
 totalSpend = 0
-date_ = None
-amount = None
-store = None
-
-categoriesDictionary = {"Entretenimiento" : 0, "Comida" : 0, "Educación" : 0, "Ropa" : 0, "Otros" : 0}
-paymentsDictionary = {"Visa": 0, "MasterCard": 0, "Paypal": 0}
-
 categoriesNames = ["Entretenimiento", "Comida", "Educación", "Ropa", "Otros"]
 paymentsNames = ["Visa", "MasterCard", "Paypal"]
-newRegister = None
-paymentAmount = []
 
 def Register(root, mainFrame):
     root.title("Register")
@@ -45,8 +35,6 @@ def Register(root, mainFrame):
     paymentsDropBox["values"] = paymentsNames
     paymentsDropBox.place(x = 145, y = 215)
 
-
-    # TODO DateEntry, Instalar tkcalendar
     Label(mainFrame, text = "Ingrese la fecha de la compra").place(x = 110, y = 260)
     global varDateEntry
     varDateEntry = DateEntry(mainFrame, selectmode ="day")
@@ -58,38 +46,21 @@ def Register(root, mainFrame):
     Entry(mainFrame, width = 25, borderwidth = 2, textvariable = storeEntry).place(x = 145, y = 355)
 
     Button(mainFrame, text = "Guardar", width = 10, command = lambda: GetRegisters(root, mainFrame)).place(x = 80, y = 450)
-    Button(mainFrame, text = "Cancelar", width = 10, command = lambda: dw.Dashboard(root, mainFrame)).place(x = 250, y = 450)
+    Button(mainFrame, text = "Cancelar", width = 10, command = lambda: dashboard_w.Dashboard(root, mainFrame)).place(x = 250, y = 450)
 
 def GetRegisters(root, mainFrame):
-    global amount 
     amount = amountEntry.get()
     category = categoriesDropBox.get()
     payment = paymentsDropBox.get()
     
     global totalSpend
     totalSpend = totalSpend + int(amount)
-
-    for name in categoriesDictionary:
-        if name == category:
-            categoriesDictionary[name] = categoriesDictionary[name] + int(amount)
-            break
-
-    for name in paymentsDictionary:
-        if name == payment:
-            paymentsDictionary[name] = paymentsDictionary[name] + int(amount)
-            break
-    global date_
-    date_ = varDateEntry.get_date()
-
-    global store
+    date_ = varDateEntry.get_date() #! Return a datetime.date
+    month = date_.month
     store = storeEntry.get()
-    
-    amount = "S/." + amount
 
-    global newRegister
-    newRegister = [date_, store, amount]
     my_path = os.getcwd()
-    file = open(my_path + r"\main\db\registers.txt", "a", encoding="UTF-8")
-    file.write(str(amount) + "," + str(category) + "," + str(payment) + "," + str(date_) + "," + str(store) + "\n")
+    file = open(my_path + r"\main\fakedb\registers.txt", "a", encoding="UTF-8")
+    file.write(str(amount) + "," + str(category) + "," + str(payment) + "," + str(date_) + "," + str(month) + "," + str(store) + "\n")
     file.close()
-    dw.Dashboard(root, mainFrame)
+    dashboard_w.Dashboard(root, mainFrame)
